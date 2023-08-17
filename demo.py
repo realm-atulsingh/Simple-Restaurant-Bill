5202245f75414d2e69251cb5b6650cc71716be5e
Atul Singh <yours.atulsingh@gmail.com>
2023-08-17 23:34:18 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+# Input two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}")
